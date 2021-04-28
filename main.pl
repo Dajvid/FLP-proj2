5d39f92071b80fc8d4a6f20be6c785a30df37405
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2021-04-28 19:34:03 +0200

    fix rotations

--- main.pl
+++ main.pl
@@ -76,9 +76,9 @@ rotate_1_up(
         [F1, T2, T3, F4, T5, T6, F7, T8, T9],
         [D1, F2, F3, D4, F5, F6, D7, F8, F9],
         [R1, R2, R3, R4, R5, R6, R7, R8, R9],
-        [T1, B2, B3, T4, B5, B6, T7, B8, B9],
+        [B1, B2, T7, B4, B5, T4, B7, B8, T1],
         [L3, L6, L9, L2, L5, L8, L1, L4, L7],
-        [B1, D2, D3, B4, D5, D6, B7, D8, D9]
+        [B9, D2, D3, B6, D5, D6, B3, D8, D9]
     ]
 ).
 
@@ -99,9 +99,9 @@ rotate_2_up(
         [T1, F2, T3, T4, F5, T6, T7, F8, T9],
         [F1, D2, F3, F4, D5, F6, F7, D8, F9],
         [R1, R2, R3, R4, R5, R6, R7, R8, R9],
-        [B1, T2, B3, B4, T5, B6, B7, T8, B9],
+        [B1, T8, B3, B4, T5, B6, B7, T2, B9],
         [L1, L2, L3, L4, L5, L6, L7, L8, L9],
-        [D1, B2, D3, D4, B5, D6, D7, B8, D9]
+        [D1, B8, D3, D4, B5, D6, D7, B2, D9]
     ]
 ).
 
@@ -122,9 +122,9 @@ rotate_3_up(
         [T1, T2, F3, T4, T5, F6, T7, T8, F9],
         [F1, F2, D3, F4, F5, D6, F7, F8, D9],
         [R7, R4, R1, R8, R5, R2, R9, R6, R3],
-        [B1, B2, T3, B4, B5, T6, B7, B8, T9],
+        [T9, B2, B3, T6, B5, B6, T3, B8, B9],
         [L1, L2, L3, L4, L5, L6, L7, L8, L9],
-        [D1, D2, B3, D4, D5, B6, D7, D8, B9]
+        [D1, D2, B7, D4, D5, B4, D7, D8, B1]
     ]
 ).
 
@@ -142,12 +142,12 @@ rotate_1_right(
         [D1, D2, D3, D4, D5, D6, D7, D8, D9]
     ],
     [
-        [L1, L2, L3, T4, T5, T6, T7, T8, T9],
+        [L7, L4, L1, T4, T5, T6, T7, T8, T9],
         [F1, F2, F3, F4, F5, F6, F7, F8, F9],
-        [T1, T2, T3, R4, R5, R6, R7, R8, R9],
+        [R1, R2, T1, R4, R5, T2, R7, R8, T3],
         [B3, B6, B9, B2, B5, B8, B1, B4, B7],
-        [D1, D2, D3, L4, L5, L6, L7, L8, L9],
-        [R1, R2, R3, D4, D5, D6, D7, D8, D9]
+        [D9, L2, L3, D8, L5, L6, D7, L8, L9],
+        [D1, D2, D3, D4, D5, D6, R9, R6, R3]
     ]
 ).
 
@@ -165,12 +165,12 @@ rotate_2_right(
         [D1, D2, D3, D4, D5, D6, D7, D8, D9]
     ],
     [
-        [T1, T2, T3, L4, L5, L6, T7, T8, T9],
+        [T1, T2, T3, L8, L5, L2, T7, T8, T9],
         [F1, F2, F3, F4, F5, F6, F7, F8, F9],
-        [R1, R2, R3, T4, T5, T6, R7, R8, R9],
+        [R1, T4, R3, R4, T5, R6, R7, T6, R9],
         [B1, B2, B3, B4, B5, B6, B7, B8, B9],
-        [L1, L2, L3, D4, D5, D6, L7, L8, L9],
-        [D1, D2, D3, R4, R5, R6, D7, D8, D9]
+        [L1, D4, L3, L4, D5, L6, L7, D6, L9],
+        [D1, D2, D3, R8, R5, R2, D7, D8, D9]
     ]
 ).
 
