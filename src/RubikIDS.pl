918c1f9c07f711c7fb0f756becd1a162565d1960
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2021-05-02 09:31:09 +0200

    remember last step and prune repetitive states

--- src/RubikIDS.pl
+++ src/RubikIDS.pl
@@ -13,9 +13,10 @@ counter(1).
 counter(X) :- counter(N), X is N + 1.
 
 
-solve_IDS(Cube, [Cube], _, _) :- solved(Cube).
-solve_IDS(Cube, [Cube | Path], DepthCounter, Depth) :- DepthCounter =< Depth,
-                                                       rotate(Cube, RotatedCube),
-                                                       solve_IDS(RotatedCube,Path, DepthCounter + 1, Depth).
+solve_IDS(Cube, [Cube], _, _, _) :- solved(Cube).
+solve_IDS(Cube, [Cube | Path], DepthCounter, Depth, Parent) :- DepthCounter =< Depth,
+                                                               rotate(Cube, RotatedCube),
+                                                               Parent \= RotatedCube,
+                                                               solve_IDS(RotatedCube, Path, DepthCounter + 1, Depth, Cube).
 
-solve_IDS(Cube, Path) :- counter(Depth), solve_IDS(Cube, Path, 0, Depth).
+solve_IDS(Cube, Path) :- counter(Depth), solve_IDS(Cube, Path, 0, Depth, none).
