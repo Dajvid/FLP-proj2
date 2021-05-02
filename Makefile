1fa68e39f7a5ba1cf69698612f684a2557538599
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2021-05-02 22:12:33 +0200

    update Makefile

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ scramble:
 	swipl -q -g main -o scramble -c cubes/RubikScramble.pl
 
 clean:
-	rm -f flp20-log scramble
+	rm -f flp20-log scramble flp-log-xsedla1d.zip
 
 zip: clean
 	zip flp-log-xsedla1d.zip Makefile src/ cubes/ README.md
--- src/RubikIDS.pl
+++ src/RubikIDS.pl
@@ -14,9 +14,10 @@ counter(X) :- counter(N), X is N + 1.
 
 
 solve_IDS(Cube, [Cube], _, _, _) :- solved(Cube).
-solve_IDS(Cube, [Cube | Path], DepthCounter, Depth, Parent) :- DepthCounter =< Depth,
-                                                               rotate(Cube, RotatedCube),
-                                                               Parent \= RotatedCube,
-                                                               solve_IDS(RotatedCube, Path, DepthCounter + 1, Depth, Cube).
+solve_IDS(Cube, [Cube | Path], DepthCounter, Depth, Parent) :-
+    DepthCounter =< Depth,
+    rotate(Cube, RotatedCube),
+    Parent \= RotatedCube,
+    solve_IDS(RotatedCube, Path, DepthCounter + 1, Depth, Cube).
 
 solve_IDS(Cube, Path) :- counter(Depth), solve_IDS(Cube, Path, 0, Depth, none).
